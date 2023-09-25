1a462e4ec05f14cf7991385b6e36d6cf1e4ac615
blacksmith <brahimhaddad.code@gmail.com>
2023-09-25 19:09:13 +0100

    add images

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class ImageController extends Controller
+{
+
+
+    public function changeAvatar(Request $request){
+        try {
+
+
+            $user = returnUserApi();
+
+            $image = new Image;
+
+            $image['url'] = $request->file('avatar')->store('public/avatar');
+
+
+            $user->images()->save($image);
+
+            return response($user, Response::HTTP_CREATED);
+
+
+
+        }catch (\Exception $e){
+            return returnResponseJson([
+                'message'=>$e->getMessage(),
+                'getCode'=>$e->getCode(),
+            ],500);
+        }
+    }}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,24 +3,89 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\ProfileResource;
+use App\Models\Image;
 use App\Models\User;
-use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\File;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
 
-    public function index(){
+
+    public function rulesUpdate(): array
+    {
+        return [
+            'name' => ['string', 'max:255'],
+            //  'email' => ['string', 'email', 'max:255', 'unique:' . User::class],
+        ];
+    }
+
+
+    public function rulesAvatar(): array
+    {
+        return [
+            'avatar' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+        ];
+
+    }
+
+    public function index()
+    {
         try {
 
             return new ProfileResource(auth('sanctum')->user());
 
         } catch (\Exception $e) {
             return returnResponseJson([
-                'message'=>$e->getMessage(),
-                'getCode'=>$e->getCode(),
-            ],500);
+                'message' => $e->getMessage(),
+                'getCode' => $e->getCode(),
+            ], 500);
         }
 
     }
 
+
+    public function update(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), $this->rulesUpdate());
+
+            if ($validator->fails()) {
+                return returnResponseJson($validator->messages(), Response::HTTP_BAD_REQUEST);
+            }
+
+            $user = returnUserApi()->update($request->all());
+
+            if ($user) {
+                return returnResponseJson([
+                    'message' => "update success",
+                ], Response::HTTP_OK);
+            }
+        } catch (\Exception $e) {
+            return returnResponseJson([
+                'message' => $e->getMessage(),
+                'getCode' => $e->getCode(),
+            ], 500);
+        }
+    }
+
+
+    public function changeAvatar(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), $this->rulesAvatar());
+
+        if ($validator->fails()) {
+            return returnResponseJson($validator->messages(), Response::HTTP_BAD_REQUEST);
+        }
+
+
+        return returnUserApi()->changeAvatar($request->file('avatar'),'images/users');
+
+//        return returnUserApi()->updateImage($request->file('avatar'),'');
+
+    }
 }
